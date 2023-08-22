16fd57264b5e05e949a8e8505256a6f76c041851
chaseseidel <chaseseidel16@gmail.com>
2023-08-21 18:53:23 -0700

    Create working app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { GamePage } from "./assets/components/GamePage";
+import { useEffect, useState } from "react";
+import "./assets/styles/reset.css";
+import "./assets/styles/style.css";
+import Loading from "./assets/components/Loading";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  });
+
   return (
     <>
-
+      {isLoading && <Loading />}
+      {!isLoading && <GamePage />}
     </>
   );
 }
